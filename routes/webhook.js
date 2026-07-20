16465f4637f8e6dbb2ed85de1fe42b752cf7ccc5
thanhmai2580 <104622508+thanhmai2580@users.noreply.github.com>
2026-07-20 10:29:38 +0700

    update

--- routes/webhook.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const webhookController = require('../controllers/webhookController');
-
-router.post('/sepay', webhookController.sepayWebhook);
-
-module.exports = router;
